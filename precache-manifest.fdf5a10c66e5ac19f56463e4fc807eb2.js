6cda0a5e2c48e2f9d28c0a259f70f72bd55cb185
Muhammad Umer <hafizumer452@gmail.com>
2020-03-08 01:21:00 +0500

    Updates

---
+++ precache-manifest.fdf5a10c66e5ac19f56463e4fc807eb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a369106247893d503d2fa3de2a707ba",
+    "url": "/domestic-solution/index.html"
+  },
+  {
+    "revision": "6cd9e300f37fc2a68f26",
+    "url": "/domestic-solution/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f79c00a87bd6db73190c",
+    "url": "/domestic-solution/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6cd9e300f37fc2a68f26",
+    "url": "/domestic-solution/static/js/2.8ac8f3bd.chunk.js"
+  },
+  {
+    "revision": "bf0033b6789b4952a1192bd985fefd8c",
+    "url": "/domestic-solution/static/js/2.8ac8f3bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f79c00a87bd6db73190c",
+    "url": "/domestic-solution/static/js/main.d4fcf1ff.chunk.js"
+  },
+  {
+    "revision": "1da5257be921e2f9788e",
+    "url": "/domestic-solution/static/js/runtime-main.fff39b57.js"
+  }
+]);
